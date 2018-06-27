c144a8906131fb86591c912d2d6384fb6b7cf073
d0dders <dodders@outlook.com>
2018-06-27 21:47:16 +0100

    Revert to proper attempt times

--- app/src/main/java/net/dodkins/pomodoro/model/AttemptKind.java
+++ app/src/main/java/net/dodkins/pomodoro/model/AttemptKind.java
@@ -1,10 +1,10 @@
 package net.dodkins.pomodoro.model;
 
 public enum AttemptKind {
-    //FOCUS(25 * 60, "Focus Time"),
-    FOCUS(10, "Focus Time"),
-    //BREAK(5 * 60, "Break Time");
-    BREAK(5, "Break Time");
+    FOCUS(25 * 60, "Focus Time"),
+    //FOCUS(10, "Focus Time"),
+    BREAK(5 * 60, "Break Time");
+    //BREAK(5, "Break Time");
 
     private int mTotalSeconds;
     private String mDisplayName;
